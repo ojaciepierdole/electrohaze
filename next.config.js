7a403ad982b43aacd1e0d591abc2a63710c1094f
ojaciepierdole <ojacie@gmail.com>
2024-11-20 14:03:17 +0100

    popraweczki

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig; 
