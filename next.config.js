5d25af182dda03d59c56ddefe5efbba2a5f0fa3d
ojaciepierdole <ojacie@gmail.com>
2025-01-16 18:45:47 +0100

    kompiluje się

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  poweredByHeader: false,
+  compress: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+    unoptimized: process.env.NODE_ENV === 'development',
+  },
   webpack: (config, { isServer }) => {
     // Dodaj fallback dla modułów node
     config.resolve.fallback = {
